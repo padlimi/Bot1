5117570efc461cab3856b6a1c953a9217ef96779
padlimi <padlim301171@gmail.com>
2026-04-07 05:42:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ MAIN_KEYBOARD = ReplyKeyboardMarkup([
 # SISTEM REMINDER CUSTOM
 # ======================================================
 custom_reminders = []
-ADMIN_PASSWORD = "Reminder23"
+ADMIN_PASSWORD = "admin123"
 
 REMINDER_KEYBOARD = ReplyKeyboardMarkup([
     ["📋 Lihat Reminder", "➕ Buat Reminder Baru"],
